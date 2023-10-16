b4d8517b498c198d1a3f80eacb16c70548fe3bc2
KyeraFrancis <KyeraFrancis@gmail.com>
2023-10-16 17:28:28 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A map that displays the location of assemblies in the United States and their affliations',
+    author='Kyera Francis',
+    license='MIT',
+)
